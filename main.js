2d1d8aa3e484ee96217b2f8959dae42029a8db87
umarrazaaa <umar.raza@invozone.com>
2022-12-09 21:43:34 +0500

    first day training

---
+++ main.js
@@ -0,0 +1,44 @@
+
+// console.log("Name Functions , Anaonymous Functions");
+
+
+// var user = {
+//     name: 'umar raza',
+//     actor : 'strange' ,
+//     yearofbirth: 1997,
+//     dateOfBirth: 26,
+//     month: 9,
+//     calculateAge: function()
+//     {
+//       console.log(  this.yearofbirth+this.dateOfBirth+this.month );
+//     }
+
+// }
+
+// console.log(user);
+
+// var yearOfBirth;
+
+// function HowOldIAm(yearOfBirth)
+// {
+//     var age = 2022 - yearOfBirth;
+
+//     return age;
+
+// }
+
+// console.log(HowOldIAm(1997));
+
+
+// var a = "global";
+// function first()
+//     {
+//         var a = "fresh";
+//         function second(){
+//         console.log(a);
+//     }
+// }
+
+// console.log(first());
+
+// Arithmatic C
---
+++ ops.js
@@ -0,0 +1,41 @@
+// Arithmatic Operators
+
+ var num = 5 ;
+
+ function calculateRemainder(inputDivider)
+ {
+    var result = 5%inputDivider;
+
+    return result;
+ }
+ // how to show decimal value here
+ console.log(calculateRemainder(98));
+
+
+ // exponential operator
+
+ console.log("exponent of 4 of 3 is " , 3 ** 4);
+
+
+ console.log( "exponent of 2 is" , (2 ** 3) ** 2);
+
+ console.log(4 ** -2);
+
+ console.log(-(2 ** 2) );
+
+ // !== means two variabls are checkd both for their value and type
+
+
+function operatorcheck()
+{
+   if(5!=5)
+   {
+     return false;
+   }
+   else{
+     return true;
+   }
+}
+
+console.log(operatorcheck());
+
