3b922795dfaf2d09de9e6971994158e419507271
umarrazaaa <umar.raza@invozone.com>
2022-12-16 15:58:29 +0500

    zoo

--- Day2/async.js
+++ Day2/async.js
@@ -1,67 +1,103 @@
-console.log("Learning async await and promises");
-console.log("A sync function always return a promise");
-console.log("Promise is resolve or reject , on that basis we cal different functions");
-console.log("async function act like a promise , either it is resolve or it will reject");
+// const radius = [5,8,3,6];
+// console.log(radius);
 
-console.log("When there is already a promis is present ");
-console.log("Then why we need to make the function async ?");
-console.log("To remove complications when we call resolve and reject methods , async functions are made");
-console.log("async function is easy to make and easy to read");
-console.log("");
-console.log("");
+// const output =  radius.map((number)=>{
 
-// let test = async function ()
-// {
-//     return "HelloWeen";
-// }
+//         return Math.PI*number*number;
+//     })
+
+//     console.log(output);
+
+const calculateArea = function(radius)
+{
+console.log("")
+const output = [];
+
+for(let i = 0; i < radius.length; i++  )
+{
+    output.push(Math.PI * radius[i] * radius[i]);
+}
+
+return output;
+};
+
+console.log(calculateArea([2]));
 
 
-// test().then((data)=>{
-//     console.log(data);
-// })
 
 
-// let test = async () => "umar";
 
-// test().then((data)=>{
-//     console.log(data);
-// });
 
+// console.log(calculateArea(radius));
+
+
+
+
+
+// console.log("Learning async await and promises");
+// console.log("A sync function always return a promise");
+// console.log("Promise is resolve or reject , on that basis we cal different functions");
+// console.log("async function act like a promise , either it is resolve or it will reject");
+
+// console.log("When there is already a promis is present ");
+// console.log("Then why we need to make the function async ?");
+// console.log("To remove complications when we call resolve and reject methods , async functions are made");
+// console.log("async function is easy to make and easy to read");
+// console.log("");
+// console.log("");
+
+// // let test = async function ()
+// // {
+// //     return "HelloWeen";
+// // }
+
+
+// // test().then((data)=>{
+// //     console.log(data);
+// // })
+
+
+// // let test = async () => "umar";
+
+// // test().then((data)=>{
+// //     console.log(data);
+// // });
+
+
+// // async function test()
+// // {
+// //    await console.log("2 : Message");
+// //   console.log("3 : Message");
+// //     console.log("4 : Message");
+// // }
+
+
+// // console.log("1 : Message");
+
+// // test();
+// // console.log("5 : Message");
 
 // async function test()
 // {
-//    await console.log("2 : Message");
-//   console.log("3 : Message");
-//     console.log("4 : Message");
+   
+//     try
+//     {
+//         const data = await fetch("users.json");
+//         const recieve = await data.json();
+//         return recieve;
+//         // return ( await fetch("users.json")).json();
+//     }catch(error)
+//     {
+//         console.log(error)
+//     }
 // }
 
-
-// console.log("1 : Message");
-
-// test();
-// console.log("5 : Message");
-
-async function test()
-{
-   
-    try
-    {
-        const data = await fetch("users.json");
-        const recieve = await data.json();
-        return recieve;
-        // return ( await fetch("users.json")).json();
-    }catch(error)
-    {
-        console.log(error)
-    }
-}
+// // test().then((data)=>{
+// //     console.log(data);
+// // });
 
 // test().then((data)=>{
 //     console.log(data);
+// }).catch((error)=>{
+//     console.log(error);
 // });
-
-test().then((data)=>{
-    console.log(data);
-}).catch((error)=>{
-    console.log(error);
-});
