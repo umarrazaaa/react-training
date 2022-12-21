50b2a398691aa6b2876fd72b5cdcecb1bea85e0c
umarrazaaa <umar.raza@invozone.com>
2022-12-22 00:27:20 +0500

    plusminus

--- plusminus.js
+++ plusminus.js
@@ -1,22 +1,31 @@
-function plusMinus(arr) {
-    let positives = 0, negatives = 0, zeros = 0;
-    const len = arr.length || 0;
-     
-    if (len > 0 && len <= 100) {
-         arr.map((elem, key) => {
-              if (elem > 0) {
-                   positives++;
-              } else if (elem < 0) {
-                   negatives++; 
-              } else {
-                   zeros++;
-              }
-                 
-              return elem; 
-         }); 
-    } 
-    
-    console.log((positives / len) || 0);
-    console.log((negatives / len) || 0);
-    console.log((zeros / len) || 0);  
+function plusMinus(arr)
+{
+     let positive_count = 0 ;
+     let negative_count = 0 ;
+     let zero_count = 0 ;
+
+     for(let i = 0; i < arr.length; i++)
+     {
+          if(arr[i]>0)
+          {
+               positive_count++;
+          }
+          else if(arr[i]<0)
+          {
+               negative_count++;
+          }
+          else
+          {
+               zero_count++;
+          }
+     }
+
+     let positive = (positive_count/arr.length).toFixed(6);
+     let negative = (negative_count/arr.length).toFixed(6);
+     let xero = (zero_count/arr.length).toFixed(6);
+
+     console.log((positive + "\n" + negative + "\n" + xero));
+
 }
+
+plusMinus([-4,3,-9,0,4,1]);
